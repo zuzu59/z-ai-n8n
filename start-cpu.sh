fb44c212c3e80d162e5958e0fa9bdbf7af6d8305
Christian Zufferey <christian@zufferey.com>
2025-11-02 09:14:37 +0100

    fixe: voilä la 1üre mouture commence ä bien fonctionner \o/

--- start-cpu.sh
+++ start-cpu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour démarrer facilement n8n avec CPU dans un container LCX
-#zf251102.0816, zf251102.0817
+#zf251102.0816, zf251102.0836
 
 # source: 
 
@@ -9,6 +9,9 @@ Démarrage de n8n...
 
 
 "
+
+mkdir n8n_storage
+chmod 777 n8n_storage
 
 #./stop.sh
 
@@ -19,6 +22,10 @@ docker compose --profile cpu up -d
 echo -e "
 
 n8n démarré !
+
+La 1ère fois, il faut attendre un moment avant de se connecter, car Ollama doit charger les modèles LLM !
+
+http://localhost:5678
 
 "
 
---
+++ stop.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#Petit script pour darrêter facilement n8n dans un container LCX
+#zf251102.0816, zf251102.0851
+
+# source: 
+
+echo -e "
+Arrêt de n8n...
+
+
+"
+
+docker compose stop
+
+
+
+echo -e "
+
+n8n arrêté !
+
+"
+
+
